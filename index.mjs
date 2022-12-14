1e2ad514716c1015c0925a5c3592cd91945c3c05
Dertex-js <ryoamir60@gmail.com>
2022-12-14 22:50:23 +0500

    game logic and ui

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import { createServer } from 'http'
 import { createServer as createSocketServer } from 'sockjs'
+import { Board } from "./lib/board.mjs"
 
 const httpServer = createServer()
 const socketServer = createSocketServer()
@@ -18,10 +19,22 @@ socketServer.on('connection', connection => {
 
             switch (parsedData.type) {
                 case 'ping':
-                    connection.write('pong')
+                    handlePing(connection)
+                    break;
+                case 'firstStep':
+                    handleFirstStep(connection, parsedData.payload)
+                    break;
+                case 'step':
+                    handleStep(connection, parsedData.payload)
+                    break;
+                case 'clearBoard':
+                    handleClear()
+                    break;
+                case 'getBoardState':
+                    handleGetBoardState(connection)
                     break;
                 default:
-                    connection.write('unknown type')
+                    handleDefault(connection)
             }
         } catch (e) {
             console.log(e)
@@ -32,3 +45,58 @@ socketServer.on('connection', connection => {
 socketServer.installHandlers(httpServer)
 
 httpServer.listen(5000)
+
+const handlePing = (connection) => {
+    connection.write(JSON.stringify({ type: 'pong' }))
+}
+
+const handleDefault = (connection) => {
+    connection.write(JSON.stringify({ type: 'unknown' }))
+}
+
+const handleFirstStep = (connection, payload) => {
+    const board = Board.getInstance()
+
+    const { result } = board?.firstStep(payload)
+
+    const boardStatus = board?.getCurrentGameState()
+
+    if (result) {
+        const message = JSON.stringify({ type: 'firstStepIsHappen', payload: boardStatus })
+        poolOfClient.forEach(conn => conn.write(message))
+    } else {
+        const message = JSON.stringify({ type: 'yourFirstStepIsFailed', payload: boardStatus })
+        connection.write(message)
+    }
+}
+
+const handleStep = (connection, payload) => {
+    const board = Board.getInstance()
+
+    const { result } = board?.step(payload)
+
+    const boardStatus = board?.getCurrentGameState()
+
+    if (result) {
+        const message = JSON.stringify({ type: 'stepIsHappen', payload: boardStatus })
+        poolOfClient.forEach(conn => conn.write(message))
+    } else {
+        const message = JSON.stringify({ type: 'yourStepIsFailed', payload: boardStatus })
+        connection.write(message)
+    }
+}
+
+const handleClear = () => {
+    const board = Board.getInstance()
+
+    board?.clear()
+
+    const message = JSON.stringify({ type: 'mapIsCleared', payload: board?.getCurrentGameState() })
+    poolOfClient.forEach(conn => conn.write(message))
+}
+
+const handleGetBoardState = (connection) => {
+    const board = Board.getInstance()
+
+    connection.write(JSON.stringify({ type: 'boardState', payload: board?.getCurrentGameState() }))
+}
